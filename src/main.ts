8985faddaefbd40fb9ba8d3dcf4dbceacab4c10a
ChristopherBraun196 <232182424+ChristopherBraun196@users.noreply.github.com>
2026-05-09 18:03:50 +0200

    add complet homescreen

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-import './styles/style.scss'
+import "./styles/style.scss";
+
+const btnPlay = document.querySelector(".btn-play");
+
+const homeScreen = document.querySelector(".home");
+const settingsScreen = document.querySelector(".settings");
+
+
+function init(){
+    btnPlay?.addEventListener('click', goToSettings);
+}
+
+function goToSettings(){
+    homeScreen?.classList.remove("screen--active");
+    settingsScreen?.classList.add("screen--active")
+}
+
+
+init()
